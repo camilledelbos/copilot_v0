1c710166db7e972a88723dc5c68cc9dcd83036fd
camilledelbos <camille.delbos@gmail.com>
2014-02-16 16:19:42 +0100

    modify root in route.rb

--- copilot_v0/config/routes.rb
+++ copilot_v0/config/routes.rb
@@ -13,7 +13,7 @@ CopilotV0::Application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'maps#index'
+  root 'landing#landing'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'

539dce3943d64f5be9072788f15edb268c82964c
camilledelbos <camille.delbos@gmail.com>
2014-02-22 16:08:00 +0100

    add Continent model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140221185219) do
+ActiveRecord::Schema.define(version: 20140222150731) do
+
+  create_table "continents", force: true do |t|
+    t.string   "continent_code"
+    t.string   "continent_name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "maps", force: true do |t|
     t.string   "map_name"
